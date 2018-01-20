4236ad88724ab4688cc97431cf0625722e95d2a1
Tobias Oberrauch <t.oberrauch@simplicity.ag>
2018-01-20 20:47:19 +0100

    Formatting

--- index.js
+++ index.js
@@ -3,28 +3,28 @@
 const {Coinpusher} = require("./lib/Coinpusher.js");
 
 if (require.main === module) {
-    
+
     const coins = process.argv[2];
-    
+
     const cp = new Coinpusher();
-    
-    switch(coins){
-    
-        case "bitcoin": 
+
+    switch (coins) {
+
+        case "bitcoin":
             cp.startForBitcoin();
-        break;
-    
+            break;
+
         case "ethereum":
             cp.startForEthereum();
-        break;
-    
+            break;
+
         case "litecoin":
             cp.startForLitecoin();
-        break;
-    
+            break;
+
         default:
             console.error("unknown coin type: " + coins);
-        break;
+            break;
     }
 
 } else {
